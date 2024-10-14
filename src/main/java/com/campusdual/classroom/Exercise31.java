7ae6fe25d4021899141f663a978afbf6ce2d74bb
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2024-10-14 20:55:48 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,28 @@
 package com.campusdual.classroom;
 
+import java.io.*;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        String cadena;
+        String archivo = "src/main/resources/lorem.txt";
+
+        FileReader f = null;
+        try {
+            f = new FileReader(archivo);
+            BufferedReader b = new BufferedReader(f);
+
+            while((cadena = b.readLine())!=null) {
+                System.out.println(cadena);
+            }
+            b.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
